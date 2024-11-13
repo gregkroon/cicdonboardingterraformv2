8512448f979aa7ed2f7d24262b56b05c6e8b8a54
gregkroon <44827446+gregkroon@users.noreply.github.com>
2024-11-13 13:07:55 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,40 @@ variable "HARNESS_PROJECT_ID" {
 variable "HARNESS_ORG_ID" {
  type        = string
  }
+variable "HARNESS_GITHUB_SECRET_VALUE" {
+  description = "The secret value for GitHub access"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_KEY" {
+  description = "The secret key for AWS"
+  type        = string
+  sensitive   = true
+}
+
+variable "HARNESS_AWS_CONNECTOR_ID" {
+  description = "The identifier for the Harness AWS connector"
+  type        = string
+}
+
+variable "AWS_ACCESS_KEY" {
+  description = "The access key for AWS"
+  type        = string
+  sensitive   = true
+}
+
+variable "HARNESS_GITHUB_CONNECTOR_ID" {
+  description = "The identifier for the Harness GitHub connector"
+  type        = string
+}
+
+variable "HARNESS_GITHUB_URL" {
+  description = "The URL for GitHub"
+  type        = string
+}
+
+variable "GITHUB_USER" {
+  description = "The GitHub username"
+  type        = string
+}
